7d2c53b27237ce462ca9acc27805ee5381cf8b74
BuildTools <mezzamat@gmail.com>
2021-02-22 16:55:57 +0100

    avancer
    nouveau groupe search result

--- Projet-S4/core/core.php
+++ Projet-S4/core/core.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
     require_once dirname(__FILE__).'/config.php';
 
@@ -11,18 +10,4 @@
         die();
     }
 
-=======
-<?php
-    require_once dirname(__FILE__).'/config.php';
-
-    # tentative de connection à la base de donnée
-
-    try {
-        $_DB = new PDO("mysql:host={$_CONFIG['db_host']};dbname={$_CONFIG['db_name']}", $_CONFIG['db_user'], $_CONFIG['db_password']);
-    } catch (PDOException $e) {
-        echo "error: database connection";
-        die();
-    }
-
->>>>>>> Stashed changes
 ?>
